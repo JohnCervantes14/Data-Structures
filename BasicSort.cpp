08a2ac3734ff467fad768be0eb7b94238dad87ca
John Cervantes <98115406+JohnCervantes14@users.noreply.github.com>
2023-03-01 16:06:58 -0600

    Update BasicSort.cpp

--- BasicSort.cpp
+++ BasicSort.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-// ---------------------------------------------------------------
-// Task 1: Define a BasicSort Class
-// ---------------------------------------------------------------
 // Below is a class for basic sorting algorithms.
 //
 // It has a private vector "list" that holds the list to sort.
-// All member functions should operate on "list".
 //
 // It has several public member functions.
 // 1. SetList: assign a vector (to sort) to "list".
@@ -21,24 +17,7 @@ using namespace std;
 // is fully sorted. If NumRound = x, then we should stop
 // when either the list is partially sorted for x rounds
 // or when the list becomes fully sorted before x rounds.
-//
-// Example 1: list={7,5,3,2}
-// BubbleSort(1), list={2,7,5,3}
-// BubbleSort(2), list={2,3,7,5}
-// BubbleSort(3), list={2,3,5,7}
-// BubbleSort(4), list={2,3,5,7}, if the list is fully sorted
-//                                before x rounds are executed,
-//                                then stop when the list is fully sorted
-// Example 2: list={7,5,3,2}
-// SelectionSort(1), list={2,5,3,7}
-// SelectionSort(2), list={2,3,5,7}
-// SelectionSort(3), list={2,3,5,7}, similarly, stop when the list is fully sorted
-//
-// Your job is to complete the following functions
-// -- SetList
-// -- GetList
-// -- BubbleSort
-// -- SelectionSort
+
 class BasicSort {
 public:
     void SetList(vector<int> InputList);
@@ -95,13 +74,12 @@ void BasicSort::SelectionSort(int NumRound) {
 // ---------------------------------------------------------------
 int main()
 {
-    // The main function is given to you.
     // We first input the testing mode, NumRound and an arbitrary number of non-
     //repeated integers into a vector.
             // The input of this list will terminate when we give an non-integer input
             //e.g., letter 'a'.
             // Then, we pass this vector to the BasicSort object.
-            // Then, we test your implemented member functions.
+            // Then, we test member functions.
             vector<int> InputList;
     vector<int> TestList;
     int temp, mode, NumRound;
