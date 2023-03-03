e133d21ebe180e2ad4396b0ef7e64c2c3d4cc114
John Cervantes <98115406+JohnCervantes14@users.noreply.github.com>
2023-03-03 13:11:57 -0600

    Update hw1_binarySearch.cpp

--- hw1_binarySearch.cpp
+++ hw1_binarySearch.cpp
@@ -9,30 +9,15 @@ class Student {
         int SID;
     };
 
-// Please define the function of SetID() here;
-// It should assign an input integer a to SID
-// ...........................................
     void Student::SetSID(int a) {
         SID = a;
     }
 
-// Please define the function of GetID() here;
-// It should return the value of SID
-// ...........................................
     int Student::GetSID() {
         return SID;
     }
 
 int* BinarySearch(Student* ptr, int arr_size, int key) {
-    // --------------------------------------------------------------------
-    // Please implement binary search here
-    // Input: an array of student objects, the array size and a search key
-    // Output: an array of two integers (declared for you below)
-    //         The 1st element is the search result: index of
-    //         the key in the array if it is found or -1
-    //         The 2nd element is the number of search iterations
-    //         it takes for your algorithm to return the result
-    // --------------------------------------------------------------------
     int keyIndex{-1};
     int iterations{0};
     int first{0};
@@ -63,8 +48,6 @@ int* BinarySearch(Student* ptr, int arr_size, int key) {
 }
 int CheckOrder(Student* ptr, int arr_size) {
     // Return 1 if sorted, and return 0 if not.
-    //
-
     // If the SID's of ptr[0], ptr[1], ptr[3] are 3, 1, 5,
     // then it is not sorted. Return 0.
     for(int i = 0; i < arr_size-1; ++i){
@@ -75,14 +58,6 @@ int CheckOrder(Student* ptr, int arr_size) {
     return 1;
 }
 int main() {
-    // The main function is given to you.
-    // It first declares an array of seven student objects.
-    // It then takes a key and seven SID's as input and
-    // assigns the ID's to the seven students.
-    //
-    // Then, it checks if the input SID's are sorted in
-    // ascending order.
-    // If so, it will perform binary search.
     Student x[7];
     int temp, key, mode, *value;
     cin >> mode >> key;
